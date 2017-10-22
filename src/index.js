3027840080a10080e425030dd1fee72b95eefb6b
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-22 14:48:04 +1000

    add import mui css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import 'semantic-ui-css/semantic.min.css'
+import 'muicss/dist/mui.min.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
