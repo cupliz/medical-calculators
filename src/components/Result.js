59baebd0bb305e539ca7b4f2a5c3035653708d4d
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-23 16:23:42 +1000

    change filling

--- src/components/Result.js
+++ src/components/Result.js
@@ -9,11 +9,6 @@ const styles = theme => ({
   card: {
     minWidth: 275
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)'
-  },
   title: {
     marginBottom: 16,
     fontSize: 14,
@@ -27,7 +22,6 @@ const styles = theme => ({
 
 const Result = props => {
   const { classes } = props
-  const bull = <span className={classes.bullet}>•</span>
 
   return (
     <div>
@@ -37,14 +31,13 @@ const Result = props => {
             Word of the Day
           </Typography>
           <Typography type='headline' component='h2'>
-            be{bull}nev{bull}o{bull}lent
+            123
           </Typography>
           <Typography type='body1' className={classes.pos}>
             adjective
           </Typography>
           <Typography component='p'>
-            well meaning and kindly.<br />
-            {'"a benevolent smile"'}
+            well meaning and kindly
           </Typography>
         </CardContent>
         <CardActions>
