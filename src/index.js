cb38fb4f1acbc2c316678aa722cd7736abf2deaf
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-23 13:54:19 +1000

    remove mui css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
-import 'muicss/dist/css/mui.min.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
