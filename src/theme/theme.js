ebec603f227a78b28abac5fa8c5a8b898aaecee8
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-23 18:50:52 +1000

    add simple docbot primary color

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,5 +1,10 @@
 import { createMuiTheme } from 'material-ui/styles'
+import { DOC_BLUE_DEFAULT } from './colors/colors'
 
-const theme = createMuiTheme()
+const theme = createMuiTheme({
+  docBot: {
+    primary: DOC_BLUE_DEFAULT
+  }
+})
 
 export default theme
