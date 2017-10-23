eef075ab15ac4f0336c9f619182a533cb3e4e255
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-23 14:10:34 +1000

    rm dirs, add empty body

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import Header from './Header/Header'
+import Header from './Header'
+import Body from './Body'
 
 const App = () => (
   <div className='app'>
     <Header />
+    <Body />
   </div>
 )
 
---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Body = () => (
+  <div>
+    Inside body
+  </div>
+)
+
+export default Body
