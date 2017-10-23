a36856d68d9af326a0fb54214f8680dd4354d427
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-23 15:05:13 +1000

    change calculate

--- src/components/Calculate.js
+++ src/components/Calculate.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Field from './Field'
 
-const Calculate = () => (
-  <div>
-    <Field />
-  </div>
-)
+class Calculate extends Component {
+  state = {
+    points: 0
+  }
+
+  handleChange = (e, value) => {
+    if (value === 0) {
+      const maxValue = parseInt(e.target.name, 10)
+      this.setState(prevState => {
+        return { points: prevState.points - maxValue }
+      })
+    } else {
+      this.setState(prevState => {
+        return { points: prevState.points + value }
+      })
+    }
+  }
+
+  render () {
+    return (
+      <div className='calculate'>
+        <div className='fields'>
+          <Field
+            label='Congestive Heart Failure History'
+            yesPoints={1}
+            onChange={this.handleChange}
+          />
+          <Field
+            label='Hypertension history'
+            yesPoints={1}
+            onChange={this.handleChange}
+          />
+          <Field
+            label='Age ≥ 75 years'
+            yesPoints={1}
+            onChange={this.handleChange}
+          />
+          <Field
+            label='Diabetes melitus history'
+            yesPoints={1}
+            onChange={this.handleChange}
+          />
+          <Field
+            label='Stroke or TIA symptoms previously'
+            yesPoints={2}
+            onChange={this.handleChange}
+          />
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Calculate
