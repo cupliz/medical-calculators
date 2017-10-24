7200334743541d1ebe1b64d5b4e90451b7638df4
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-24 17:03:05 +1000

    use content as string to prevent XSS

--- src/components/References.js
+++ src/components/References.js
@@ -4,30 +4,32 @@ import ClosableCard from './ClosableCard'
 const data = [
   {
     title: 'Notes',
-    content: <ul><li>Estimates stroke risk in patients with atrial fibrillation</li>
+    content: `Estimates stroke risk in patients with atrial fibrillation
 
-      <li>The CHADS2 score is one of several risk stratification schemes that can help determine the 1 year risk of an ischemic stroke in a non-anticoagulated patient with non-valvular atrial fibrillation and determine which antithrombotic therapy is most appropriate</li>
+The CHADS2 score is one of several risk stratification schemes that can help determine the 1 year risk of an ischemic stroke in a non-anticoagulated patient with non-valvular atrial fibrillation and determine which antithrombotic therapy is most appropriate
 
-      <li>The CHA2-DS2-VASC score may be a better tool to assess stroke risk in patients with atrial fibrillation.  It risk stratifies these patients better than the CHADS2 score</li></ul>
+The CHA2-DS2-VASC score may be a better tool to assess stroke risk in patients with atrial fibrillation.  It risk stratifies these patients better than the CHADS2 score
+  `
   },
   {
     title: 'References',
-    content: 'Gage BF, Waterman AD, Shannon W, et. al. Validation of clinical classification schemes for predicting stroke: results from the National Registry of Atrial Fibrillation, JAMA, 2001 Jun 13;285(22):2864-70'
+    content:
+      'Gage BF, Waterman AD, Shannon W, et. al. Validation of clinical classification schemes for predicting stroke: results from the National Registry of Atrial Fibrillation, JAMA, 2001 Jun 13;285(22):2864-70'
   }
 ]
 
 const renderCards = () => {
   return data.map(item => {
     return (
-      <ClosableCard key={item.title} title={item.title} content={item.content} />
+      <ClosableCard
+        key={item.title}
+        title={item.title}
+        content={item.content}
+      />
     )
   })
 }
 
-const References = () => (
-  <div>
-    {renderCards()}
-  </div>
-)
+const References = () => <div>{renderCards()}</div>
 
 export default References
