1fd5146b20faa091e98ddd71d34d8149fbba8302
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-24 13:52:50 +1000

    add references card

--- src/components/References.js
+++ src/components/References.js
@@ -10,6 +10,10 @@ const data = [
       <li>The CHADS2 score is one of several risk stratification schemes that can help determine the 1 year risk of an ischemic stroke in a non-anticoagulated patient with non-valvular atrial fibrillation and determine which antithrombotic therapy is most appropriate</li>
 
       <li>The CHA2-DS2-VASC score may be a better tool to assess stroke risk in patients with atrial fibrillation.  It risk stratifies these patients better than the CHADS2 score</li></ul>
+  },
+  {
+    title: 'References',
+    content: 'Gage BF, Waterman AD, Shannon W, et. al. Validation of clinical classification schemes for predicting stroke: results from the National Registry of Atrial Fibrillation, JAMA, 2001 Jun 13;285(22):2864-70'
   }
 ]
 
