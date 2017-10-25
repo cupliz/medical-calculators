453c20527682bb8a19c67605ddf6c1753beb0f7c
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-25 16:43:40 +1000

    add body styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { MuiThemeProvider } from 'material-ui/styles'
 import 'normalize.css'
 import theme from './theme/theme'
+import './index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
