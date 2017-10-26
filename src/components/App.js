f5aefbf46b2aa5ef794fe7304ea10c6c093cd5f8
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-26 18:48:26 +1000

    import data

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from './Header'
 import Body from './Body'
 import Result from './Result'
+import data from './data.json'
 
 const App = () => (
   <div className='app'>
     <Header />
     <Body />
-    <Result points={123} data={data[this.state.points]} />
+    <Result points={1} data={data[1]} />
   </div>
 )
 
