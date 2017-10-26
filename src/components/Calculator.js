df560c45e8b28b5ed446f54d50c668981c81dceb
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-26 18:52:07 +1000

    App -> Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,12 +4,12 @@ import Body from './Body'
 import Result from './Result'
 import data from './data.json'
 
-const App = () => (
-  <div className='app'>
+const Calculator = () => (
+  <div className='calculator'>
     <Header />
     <Body />
     <Result points={1} data={data[1]} />
   </div>
 )
 
-export default App
+export default Calculator
--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import { MuiThemeProvider } from 'material-ui/styles'
 import 'normalize.css'
 import theme from './theme/theme'
 import './index.css'
-import App from './components/App'
+import Calculator from './components/Calculator'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
-    <App />
+    <Calculator />
   </MuiThemeProvider>,
   document.getElementById('root')
 )
