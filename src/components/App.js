573571d243d06595f58f318fa8b193d415d4edbe
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-26 18:46:51 +1000

    extract calculate and data

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import Body from './Body'
+import Result from './Result'
 
 const App = () => (
   <div className='app'>
     <Header />
     <Body />
+    <Result points={123} data={data[this.state.points]} />
   </div>
 )
 
