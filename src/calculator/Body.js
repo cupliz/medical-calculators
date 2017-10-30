7d50226d905671a42ee5f7df70cef65ce0909971
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-30 12:38:26 +1000

    mv body

--- src/calculator/Body.js
+++ src/calculator/Body.js
@@ -4,8 +4,8 @@ import { withStyles } from 'material-ui/styles'
 import SwipeableViews from 'react-swipeable-views'
 import AppBar from 'material-ui/AppBar'
 import Tabs, { Tab } from 'material-ui/Tabs'
-import Calculate from './Calculate'
-import References from './References'
+import Calculate from '../components/Calculate'
+import References from '../components/References'
 
 const TabContainer = ({ children, dir }) => {
   return <div dir={dir}>{children}</div>
--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './Header'
-import Body from '../components/Body'
+import Body from './Body'
 
 const Calculator = () => (
   <div className='calculator'>
