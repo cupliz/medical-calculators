150a2fe0ee4fd6b9880c8bf0118d3af2903f8182
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-30 20:11:44 +1000

    clean Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,5 @@
 import React from 'react'
 
-import { push } from 'react-router-redux'
-import { connect } from 'react-redux'
+const Home = props => <div>Some Home content inside</div>
 
-
-const Home = props => (
-  <div>
-    <button onClick={() => props.changePage()}>
-      Go to about page via redux
-    </button>
-  </div>
-)
-
-const mapStateToProps = state => ({
-  calculator: state.calculator
-})
-
-const mapDispatchToProps = {
-  changePage: path => push(`/chad2-calc`)
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default Home
