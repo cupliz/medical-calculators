3e213dfd69b87edc3f1497c6194820cb4fdd04dc
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-30 14:55:17 +1000

    add empty space

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link, Route } from 'react-router-dom'
+import Calculator from './calculator/Calculator'
 
 const App = () => (
   <div>
-    Inside App
+    <header>
+      <Link to='/'>Home</Link> {}
+      <Link to='/chad2-calc'>Calculator</Link>
+    </header>
+
+    <main>
+      <Route exact path='/chad2-calc' component={Calculator} />
+    </main>
   </div>
 )
 
