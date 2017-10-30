a2f096fd7848e36533d22f2da5d8ab03c36b4eea
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-30 18:18:37 +1000

    add 2 fetch to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link, Route } from 'react-router-dom'
 import Calculator from './calculator/Calculator'
+import { connect } from 'react-redux'
+import { fetchCalcDataAxios, fetchCalcDataFetch } from '../store/modules/calculator'
 
-const App = () => (
+const App = props => (
   <div>
     <header>
       <Link to='/'>Home</Link> {}
       <Link to='/chad2-calc'>Calculator</Link>
     </header>
+
+    <button onClick={props.fetchCalcDataFetch}>Fetch using Fetch</button>
+    <button onClick={props.fetchCalcDataAxios}>Fetch using Axios</button>
 
     <main>
       <Route exact path='/chad2-calc' component={Calculator} />
@@ -15,4 +20,13 @@ const App = () => (
   </div>
 )
 
-export default App
+const mapStateToProps = state => ({
+  calculator: state.calculator
+})
+
+const mapDispatchToProps = {
+  fetchCalcDataFetch,
+  fetchCalcDataAxios
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
