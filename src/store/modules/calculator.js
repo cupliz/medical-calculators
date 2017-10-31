937a7e23c338799a116a08dac18ca3d32003102a
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-31 14:44:12 +1000

    format

--- src/store/modules/calculator.js
+++ src/store/modules/calculator.js
@@ -112,7 +112,11 @@ export const fetchCalcData = () => {
           // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
           // http.ClientRequest in node.js
           // console.log(error.request)
-          dispatch(fetchCalcDataError('The request was made but no response was received'))
+          dispatch(
+            fetchCalcDataError(
+              'The request was made but no response was received'
+            )
+          )
         } else {
           // Something happened in setting up the request that triggered an Error
           // console.log('Error', error.message)
