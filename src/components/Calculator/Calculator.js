a8dd31c94f4878eb49bf8c403978164e2d14ca41
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-31 12:50:18 +1000

    add connect to calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CalculatorHeader from './header/CalculatorHeader'
 import CalculatorBody from './body/CalculatorBody'
+import { connect } from 'react-redux'
 
 const Calculator = () => (
   <div className='calculator'>
@@ -9,4 +10,10 @@ const Calculator = () => (
   </div>
 )
 
-export default Calculator
+const mapStateToProps = state => ({
+})
+
+const mapDispatchToProps = {
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Calculator)
