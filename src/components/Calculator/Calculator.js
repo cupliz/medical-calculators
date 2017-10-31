55d66051345ecd7fa097b0327e6bcbf5c89b80fe
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-31 12:54:21 +1000

    add fetch

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import CalculatorHeader from './header/CalculatorHeader'
 import CalculatorBody from './body/CalculatorBody'
 import { connect } from 'react-redux'
+import { fetchCalcData } from '../../store/modules/calculator'
 
-const Calculator = () => (
-  <div className='calculator'>
-    <CalculatorHeader />
-    <CalculatorBody />
-  </div>
-)
+class Calculator extends Component {
+  componentDidMount () {
+    this.props.fetchCalcData()
+  }
+
+  render () {
+    return (
+      <div className='calculator'>
+        <CalculatorHeader />
+        <CalculatorBody />
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = state => ({
+  calculators: state.data
 })
 
 const mapDispatchToProps = {
+  fetchCalcData
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Calculator)
