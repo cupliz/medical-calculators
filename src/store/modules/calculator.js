eb85ef2536b30fc7a74b93dbfb3a4742a5af70ff
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-31 14:41:32 +1000

    reformat and fix catching

--- src/store/modules/calculator.js
+++ src/store/modules/calculator.js
@@ -87,40 +87,38 @@ export const pickAnswerError = (errorMessage = '') => {
 export const fetchCalcData = () => {
   return (dispatch, getState) => {
     dispatch(fetchCalcDataRequest())
-    const url = `http://localhost:3001/calculators/`
-    return (
-      axios
-        .get(url)
-        .then(({ data }) => {
-          dispatch(fetchCalcDataSuccess(data))
-          dispatch(fetchCalcDataRequest(true))
-        })
-        .catch(error => {
-          dispatch(fetchCalcDataRequest(false))
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            // console.log(error.response.data);
-            // console.log(error.response.status);
-            // console.log(error.response.headers);
-            dispatch(
-              fetchCalcDataError(
-                `${error.response.status} ${error.response.statusText}`
-              )
+    const url = `http://localhost:3002/calculators/`
+    return axios
+      .get(url)
+      .then(({ data }) => {
+        dispatch(fetchCalcDataSuccess(data))
+        dispatch(fetchCalcDataRequest(true))
+      })
+      .catch(error => {
+        dispatch(fetchCalcDataRequest(false))
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          // console.log(error.response.data);
+          // console.log(error.response.status);
+          // console.log(error.response.headers);
+          dispatch(
+            fetchCalcDataError(
+              `${error.response.status} ${error.response.statusText}`
             )
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            // console.log(error.request)
-            fetchCalcDataError(error.request)
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            // console.log('Error', error.message)
-            fetchCalcDataError(`Error: ${error.message}`)
-          }
-        })
-    )
+          )
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          // console.log(error.request)
+          dispatch(fetchCalcDataError(error.request))
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          // console.log('Error', error.message)
+          dispatch(fetchCalcDataError(`Error: ${error.message}`))
+        }
+      })
   }
 }
 
