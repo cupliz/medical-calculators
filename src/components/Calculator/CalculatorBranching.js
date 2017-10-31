f9d1360914cbe841b5be16318f7d7aa495b46aac
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-31 13:53:28 +1000

    add calculator branching render props

---
+++ src/components/Calculator/CalculatorBranching.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+const LoadingView = () => <div>Loading...</div>
+
+const ErrorView = errorMessage => <div>I'm sorry! Error: ${errorMessage}</div>
+
+const ComponentView = props => <div>Inside ComponentView</div>
+
+class Calculator extends Component {
+  state = { loading: true }
+
+  componentDidMount () {
+    // do fetch there
+  }
+
+  render () {
+    if (this.state.loading) {
+      return this.props.renderLoading()
+    } else if (this.state.planet) {
+      return this.props.renderPlanet(this.state.planet)
+    } else {
+      return this.props.renderError(this.state.error)
+    }
+  }
+}
+
+export default () => (
+  <Calculator
+    renderLoading={() => <LoadingView />}
+    renderError={errorMessage => <ErrorView />}
+    renderPlanet={planet => <ComponentView {...planet} />}
+  />
+)
