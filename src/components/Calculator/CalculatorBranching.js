621e4c44a016eb66da69451bd923188fe6867075
Govard Barkhatov <gbarkhatov@gmail.com>
2017-10-31 14:20:39 +1000

    remove render props and use component

--- src/components/Calculator/CalculatorBranching.js
+++ src/components/Calculator/CalculatorBranching.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react'
+import CalculatorHeader from './header/CalculatorHeader'
+import CalculatorBody from './body/CalculatorBody'
+import { connect } from 'react-redux'
+import { fetchCalcData } from '../../store/modules/calculator'
 
 const LoadingView = () => <div>Loading...</div>
 
-const ErrorView = errorMessage => <div>I'm sorry! Error: ${errorMessage}</div>
+const ErrorView = () => <div>I'm sorry! Please try again.</div>
 
-const ComponentView = props => <div>Inside ComponentView</div>
+const PlanetView = ({ name, climate, terrain }) => (
+  <div>
+    <h2>{name}</h2>
+    <div>Climate: {climate}</div>
+    <div>Terrain: {terrain}</div>
+  </div>
+)
+
+const PlanetBranch = ({ loading, planet }) => {
+  if (loading) {
+    return <LoadingView />
+  } else if (planet) {
+    return <PlanetView {...planet} />
+  } else {
+    return <ErrorView />
+  }
+}
 
 class Calculator extends Component {
   state = { loading: true }
 
   componentDidMount () {
-    console.log(1)
+    fetch('https://swapi.co/api/planets/5')
+      .then(res => res.json())
+      .then(
+        planet => this.setState({ loading: false, planet }),
+        error => this.setState({ loading: false, error })
+      )
   }
 
   render () {
-    if (this.state.loading) {
-      return this.props.renderLoading()
-    } else if (this.state.planet) {
-      return this.props.renderComponent(this.state.data)
-    } else {
-      return this.props.renderError(this.state.error)
-    }
+    return <PlanetBranch {...this.state} />
   }
 }
 
-export default () => (
-  <Calculator
-    renderLoading={() => <LoadingView />}
-    renderError={errorMessage => <ErrorView />}
-    renderComponent={data => <ComponentView {...data} />}
-  />
-)
+export default Calculator
