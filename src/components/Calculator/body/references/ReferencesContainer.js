d0ea44b27688d3ff414ed18c53978439f3a59716
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-01 16:07:25 +1000

    rm pretyped data in references container

--- src/components/Calculator/body/references/ReferencesContainer.js
+++ src/components/Calculator/body/references/ReferencesContainer.js
@@ -1,27 +1,6 @@
 import React from 'react'
 import InfoCard from './InfoCard'
 
-const data = [
-  {
-    id: 1,
-    title: 'Notes',
-    type: 'unordered-list',
-    content: [
-      'Estimates stroke risk in patients with atrial fibrillation',
-      'The CHADS2 score is one of several risk stratification schemes that can help determine the 1 year risk of an ischemic stroke in a non-anticoagulated patient with non-valvular atrial fibrillation and determine which antithrombotic therapy is most appropriate',
-      'The CHA2-DS2-VASC score may be a better tool to assess stroke risk in patients with atrial fibrillation.  It risk stratifies these patients better than the CHADS2 score'
-    ]
-  },
-  {
-    id: 2,
-    title: 'References',
-    type: 'ordered-list',
-    content: [
-      'Gage BF, Waterman AD, Shannon W, et. al. Validation of clinical classification schemes for predicting stroke: results from the National Registry of Atrial Fibrillation. JAMA. 2001 Jun 13;285(22):2864-70.',
-      'Go AS, Hylek EM, Chang Y, et. al. Anticoagulation therapy for stroke prevention in atrial fibrillation: how well do randomized trials translate into clinical practice?. JAMA. 2003 Nov 26;290(20):2685-92.'
-    ]
-  }
-]
 const ReferencesContainer = props => (
   <div>
     <InfoCard
