8679ab052280a8b14c157c8024143978ce352927
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-01 14:15:54 +1000

    add list reducer

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import calculator from './calculator'
+import list from './list'
 
 export default combineReducers({
   router: routerReducer,
-  calculator
+  calculator,
+  list
 })
