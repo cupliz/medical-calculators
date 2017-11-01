a2bd789054f7686be32b9166c14978295d06c9b5
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-01 16:08:08 +1000

    refactor

--- src/components/Calculator/body/references/ReferencesContainer.js
+++ src/components/Calculator/body/references/ReferencesContainer.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import InfoCard from './InfoCard'
 
-const ReferencesContainer = props => (
-  <div>
-    <InfoCard
-      title='Notes'
-      type={props.notes.type}
-      content={props.notes.content}
-    />
-    <InfoCard
-      title='References'
-      type={props.references.type}
-      content={props.references.content}
-    />
-  </div>
-)
+const ReferencesContainer = props => {
+  return (
+    <div>
+      <InfoCard
+        title='Notes'
+        type={props.notes.type}
+        content={props.notes.content}
+      />
+      <InfoCard
+        title='References'
+        type={props.references.type}
+        content={props.references.content}
+      />
+    </div>
+  )
+}
 
 export default ReferencesContainer
