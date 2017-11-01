ad0f78ceadf7d548632b28d885daae08ac62da87
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-01 14:25:28 +1000

    pass down fetch and list to app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link, Route, withRouter } from 'react-router-dom'
 import Home from './components/Home/Home'
 import Calculator from './components/Calculator/Calculator'
 import { connect } from 'react-redux'
+import { fetchListData } from './store/modules/list'
 
-const App = props => (
-  <main>
-    <header>
-      <Link to='/'>Home</Link> {' - '}
-      <Link to='/chad2-calc'>CHADS2 Score for Atrial Fibrillation</Link>
-    </header>
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <header>
+          <Link to="/">Home</Link> {' - '}
+          <Link to="/chad2-calc">CHADS2 Score for Atrial Fibrillation</Link>
+        </header>
 
-    <Route exact path='/' component={Home} />
-    <Route exact path='/chad2-calc' component={Calculator} />
-  </main>
-)
+        <Route exact path="/" component={Home} />
+        <Route exact path="/chad2-calc" component={Calculator} />
+      </main>
+    )
+  }
+}
 
-export default withRouter(connect()(App))
+const mapStateToProps = state => ({
+  list: state.list
+})
+
+const mapDispatchToProps = {
+  fetchListData
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
