aa50f93093a955236a20b06e8d0f2bb638fdc660
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-05 16:36:04 +1000

    add RadioField

---
+++ src/components/Calculator/body/calculate/fields/Radio.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Radio, { RadioGroup } from 'material-ui/Radio'
+import { FormLabel, FormControl, FormControlLabel } from 'material-ui/Form'
+
+const RadioField = props => (
+  <RadioGroup
+    row
+    aria-label={this.props.label}
+    name={answerPoints[1]}
+    className={classes.group}
+    value={this.state.value}
+    onChange={this.handleChange}
+  >
+    <FormControlLabel
+      value={answerPoints[0]}
+      className={classes.formControlLabel}
+      control={<Radio checked={!this.state.checked} />}
+      label={
+        <span>
+          No{' '}
+          <small
+            className={
+              this.state.checked ? classes.unCheckedBadge : classes.checkedBadge
+            }
+          >
+            {answerPoints[0]}
+          </small>
+        </span>
+      }
+    />
+    <FormControlLabel
+      value={answerPoints[1]}
+      className={classes.formControlLabel}
+      control={<Radio checked={this.state.checked} />}
+      label={
+        <span>
+          Yes{' '}
+          <small
+            className={
+              !this.state.checked
+                ? classes.unCheckedBadge
+                : classes.checkedBadge
+            }
+          >
+            +{answerPoints[1]}
+          </small>
+        </span>
+      }
+    />
+  </RadioGroup>
+)
+
+export default RadioField
