0929d6fc393eab3899bdc9c474a3a6e3b1f82ffa
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-05 16:57:16 +1000

    add checkbox field

---
+++ src/components/Calculator/body/calculate/fields/CheckboxField.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import Radio, { RadioGroup } from 'material-ui/Radio'
+import { FormControlLabel } from 'material-ui/Form'
+
+const RadioField = props => {
+  const { answerPoints, label, classes, value, onChange, checked } = props
+  return (
+    <RadioGroup
+      row
+      name={answerPoints[1]}
+      aria-label={label}
+      className={classes.group}
+      value={value}
+      onChange={onChange}
+    >
+      <FormControlLabel
+        value={answerPoints[0]}
+        className={classes.formControlLabel}
+        control={<Radio checked={!checked} />}
+        label={
+          <span>
+            No{' '}
+            <small
+              className={
+                checked
+                  ? classes.unCheckedBadge
+                  : classes.checkedBadge
+              }
+            >
+              {answerPoints[0]}
+            </small>
+          </span>
+        }
+      />
+      <FormControlLabel
+        value={answerPoints[1]}
+        className={classes.formControlLabel}
+        control={<Radio checked={checked} />}
+        label={
+          <span>
+            Yes{' '}
+            <small
+              className={
+                !checked
+                  ? classes.unCheckedBadge
+                  : classes.checkedBadge
+              }
+            >
+              +{answerPoints[1]}
+            </small>
+          </span>
+        }
+      />
+    </RadioGroup>
+  )
+}
+
+export default RadioField
