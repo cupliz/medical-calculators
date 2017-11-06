e435f3c58638d9b25177a30ae37fac2e8391469e
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-06 15:01:59 +1000

    change margins

--- src/components/FOF/FOF.js
+++ src/components/FOF/FOF.js
@@ -11,14 +11,14 @@ const styles = theme => ({
     justifyContent: 'center'
   },
   headline: {
-    marginBottom: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 4,
     fontWeight: 'bold'
   },
   title: {
-    marginBottom: theme.spacing.unit * 2
+    marginBottom: theme.spacing.unit * 3
   },
   caption: {
-    marginBottom: theme.spacing.unit * 2
+    marginBottom: theme.spacing.unit * 3
   }
 })
 
