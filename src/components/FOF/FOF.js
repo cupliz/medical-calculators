a085ce9a6402cd92eba3d3874f6dfe50e503100d
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-06 14:53:43 +1000

    add components

--- src/components/FOF/FOF.js
+++ src/components/FOF/FOF.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import Typography from 'material-ui/Typography'
 
-const FOF = props => <p>Not Found</p>
+const FOF = props => (
+  <div>
+    <Typography type='headline' gutterBottom>
+      Oops!
+    </Typography>
+    <Typography type='title' gutterBottom>
+      We can’t seem to find the page you are looking for.
+    </Typography>
+    <Typography type='caption' gutterBottom align='center'>
+      Error code 404
+    </Typography>
+  </div>
+)
 
 export default FOF
