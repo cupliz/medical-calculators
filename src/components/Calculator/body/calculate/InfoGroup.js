f648b8b61a65836254e380f46ca576c1e692d633
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-08 14:12:30 +1000

    import withStyles and PropTypes

--- src/components/Calculator/body/calculate/InfoGroup.js
+++ src/components/Calculator/body/calculate/InfoGroup.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { withStyles } from 'material-ui/styles'
+import PropTypes from 'prop-types'
 
 const InfoGroup = props => (
   <div>
