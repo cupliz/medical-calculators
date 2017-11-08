532f36e930efdf30d883b50910807b9b2f990c77
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-08 14:13:04 +1000

    add few styles

--- src/components/Calculator/body/calculate/InfoGroup.js
+++ src/components/Calculator/body/calculate/InfoGroup.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react'
 import { withStyles } from 'material-ui/styles'
 import PropTypes from 'prop-types'
 
+const styles = theme => ({
+  questionGroup: {
+    display: 'flex',
+    borderBottom: `1px solid ${theme.brand.colors.separatorGrey}`,
+    borderTop: `1px solid ${theme.brand.colors.separatorGrey}`,
+    backgroundColor: theme.brand.colors.greyBg
+  },
+  formControl: {
+    margin: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 2}px 0 ${theme
+      .spacing.unit * 2}px`
+  }
+})
+
 const InfoGroup = props => (
   <div>
     Inside InfoGroup
