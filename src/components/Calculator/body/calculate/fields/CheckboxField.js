5c14ce744b4db56a1359c71ccd9c5e9b73092d6a
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-12 20:42:23 +1000

    convert checkbox to class

--- src/components/Calculator/body/calculate/fields/CheckboxField.js
+++ src/components/Calculator/body/calculate/fields/CheckboxField.js
@@ -1,45 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { FormGroup, FormControlLabel } from 'material-ui/Form'
 import Checkbox from 'material-ui/Checkbox'
 
-const CheckboxField = props => {
-  const {
-    answerPoints,
-    ariaLabel,
-    classes,
-    value,
-    onChange,
-    checked,
-    questionLabel
-  } = props
-  return (
-    <FormGroup row aria-label={ariaLabel} className={classes.group}>
-      <FormControlLabel
-        value={answerPoints[0]}
-        className={classes.formControlLabel}
-        control={
-          <Checkbox
-            value={value}
-            onChange={() => onChange(answerPoints[1])}
-            name={answerPoints[1]}
-            checked={checked}
-          />
-        }
-        label={
-          <span>
-            {questionLabel}
-            <small
-              className={
-                checked ? classes.checkedBadge : classes.unCheckedBadge
-              }
-            >
-              {`${answerPoints[1]} Point`}
-            </small>
-          </span>
-        }
-      />
-    </FormGroup>
-  )
+class CheckboxField extends Component {
+  state = {
+    checked: false,
+    value: '0'
+  }
+
+  handleChange = points => {
+    console.log(points)
+  }
+
+  render () {
+    const { answerPoints, questionLabel, group, classes } = this.props
+    return (
+      <FormGroup row aria-label={group} className={classes.group}>
+        <FormControlLabel
+          value={answerPoints[0]}
+          className={classes.formControlLabel}
+          control={
+            <Checkbox
+              value={this.state.value}
+              onChange={() => this.handleChange(answerPoints[1])}
+              name={answerPoints[1]}
+              checked={this.state.checked}
+            />
+          }
+          label={
+            <span>
+              {questionLabel}
+              <small
+                className={
+                  this.state.checked
+                    ? classes.checkedBadge
+                    : classes.unCheckedBadge
+                }
+              >
+                {`${answerPoints[1]} Point`}
+              </small>
+            </span>
+          }
+        />
+      </FormGroup>
+    )
+  }
 }
 
 export default CheckboxField
