a283bd44451b7156811e102f3094b9c2d890a40c
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-12 20:24:34 +1000

    convert to class

--- src/components/Calculator/body/calculate/fields/RadioField.js
+++ src/components/Calculator/body/calculate/fields/RadioField.js
@@ -1,56 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Radio, { RadioGroup } from 'material-ui/Radio'
 import { FormControlLabel } from 'material-ui/Form'
 
-const RadioField = props => {
-  const { answerOptions, answerPoints, group, classes } = props
-  return (
-    <RadioGroup
-      row
-      name={answerPoints[1]}
-      aria-label={group}
-      className={classes.group}
-    >
-      <FormControlLabel
-        value={answerPoints[0]}
-        className={classes.formControlLabel}
-        control={<Radio checked={!checked} />}
-        label={
-          <span>
-            {answerOptions[0] }
-            <small
-              className={
-                checked
-                  ? classes.unCheckedBadge
-                  : classes.checkedBadge
-              }
-            >
-              {answerPoints[0]}
-            </small>
-          </span>
-        }
-      />
-      <FormControlLabel
-        value={answerPoints[1]}
-        className={classes.formControlLabel}
-        control={<Radio checked={checked} />}
-        label={
-          <span>
-            {answerOptions[1] }
-            <small
-              className={
-                !checked
-                  ? classes.unCheckedBadge
-                  : classes.checkedBadge
-              }
-            >
-              +{answerPoints[1]}
-            </small>
-          </span>
-        }
-      />
-    </RadioGroup>
-  )
+class RadioField extends Component {
+  state = {
+    checked: false
+  }
+
+  render () {
+    const { answerOptions, answerPoints, group, classes } = this.props
+    return (
+      <RadioGroup
+        row
+        name={answerPoints[1]}
+        aria-label={group}
+        className={classes.group}
+      >
+        <FormControlLabel
+          value={answerPoints[0]}
+          className={classes.formControlLabel}
+          control={<Radio checked={!this.state.checked} />}
+          label={
+            <span>
+              {answerOptions[0]}
+              <small
+                className={
+                  this.state.checked
+                    ? classes.unCheckedBadge
+                    : classes.checkedBadge
+                }
+              >
+                {answerPoints[0]}
+              </small>
+            </span>
+          }
+        />
+        <FormControlLabel
+          value={answerPoints[1]}
+          className={classes.formControlLabel}
+          control={<Radio checked={this.state.checked} />}
+          label={
+            <span>
+              {answerOptions[1]}
+              <small
+                className={
+                  !this.state.checked
+                    ? classes.unCheckedBadge
+                    : classes.checkedBadge
+                }
+              >
+                +{answerPoints[1]}
+              </small>
+            </span>
+          }
+        />
+      </RadioGroup>
+    )
+  }
 }
 
 export default RadioField
