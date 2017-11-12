e3f2d4c5423600cc11ebc1a17dac0249ab4a813e
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-12 19:02:58 +1000

    simplified calculate container

--- src/components/Calculator/body/calculate/CalculateContainer.js
+++ src/components/Calculator/body/calculate/CalculateContainer.js
@@ -2,10 +2,6 @@ import React from 'react'
 import QuestionGroup from './QuestionGroup'
 import InfoGroup from './InfoGroup'
 
-const renderInfoGroup = info => {
-  return <InfoGroup {...info} />
-}
-
 const renderQuestionGroups = questions => {
   return questions.map(questionGroup => {
     return (
@@ -21,7 +17,7 @@ const CalculateContainer = props => {
   const { info, questions } = props
   return (
     <div className='calculateContainer'>
-      {info && renderInfoGroup(info)}
+      {info && <InfoGroup {...info} />}
       {renderQuestionGroups(questions)}
     </div>
   )
