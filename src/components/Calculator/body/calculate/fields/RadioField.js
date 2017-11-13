800b8c49f44007439b9a5457986a94f095fd82e2
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-13 14:06:07 +1000

    extract pickRadioAnswer

--- src/components/Calculator/body/calculate/fields/RadioField.js
+++ src/components/Calculator/body/calculate/fields/RadioField.js
@@ -10,15 +10,15 @@ class RadioField extends Component {
   }
 
   handleChange = (e, value) => {
-    const { points, answerPoints, answerOptions, group } = this.props
+    const { points, answerPoints, answerOptions, group, pickRadioAnswer } = this.props
     let checked = false
     if (value === answerPoints[0]) {
       const decreasedValue = points - parseInt(answerPoints[1], 10)
-      this.props.pickRadioAnswer(group, answerOptions[0], decreasedValue, value)
+      pickRadioAnswer(group, answerOptions[0], decreasedValue, value)
     } else if (value === answerPoints[1]) {
       checked = true
       const increasedValue = points + parseInt(answerPoints[1], 10)
-      this.props.pickRadioAnswer(group, answerOptions[1], increasedValue, value)
+      pickRadioAnswer(group, answerOptions[1], increasedValue, value)
     }
     this.setState({ value, checked })
   }
