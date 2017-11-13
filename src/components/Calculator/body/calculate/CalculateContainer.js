cb29b2ff110436f0c9f43e1107ace0e30a01ef17
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-13 14:11:12 +1000

    used results card

--- src/components/Calculator/body/calculate/CalculateContainer.js
+++ src/components/Calculator/body/calculate/CalculateContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import QuestionGroup from './QuestionGroup'
 import InfoGroup from './InfoGroup'
 import ResultCard from './ResultCard'
+import { connect } from 'react-redux'
 
 const renderQuestionGroups = questions => {
   return questions.map(questionGroup => {
@@ -18,8 +19,13 @@ const CalculateContainer = props => (
   <div className='calculateContainer'>
     {props.info && <InfoGroup {...props.info} />}
     {renderQuestionGroups(props.questions)}
-    <ResultCard points={points} data={results[points]} />
+    <ResultCard points={props.points} data={props.data} />
   </div>
 )
 
-export default CalculateContainer
+const mapStateToProps = state => ({
+  points: state.calculator.data.points,
+  data: state.calculator.data.results[state.calculator.data.points]
+})
+
+export default connect(mapStateToProps)(CalculateContainer)
