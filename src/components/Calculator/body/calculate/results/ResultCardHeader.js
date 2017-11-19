2ed7b119d129a305cff58bdf2fe93fd7ae783001
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-19 21:04:35 +1000

    extract in result card header properly

--- src/components/Calculator/body/calculate/results/ResultCardHeader.js
+++ src/components/Calculator/body/calculate/results/ResultCardHeader.js
@@ -4,24 +4,21 @@ import IconButton from 'material-ui/IconButton'
 import ShareIcon from 'material-ui-icons/Share'
 import Typography from 'material-ui/Typography'
 
-export const ResultCardHeader = props => {
-  const { classes } = props
-  return (
-    <Card className={classes.card}>
-      <CardHeader
-        className={classes.header}
-        title={
-          <div className={classes.titleWrapper}>
-            <Typography type='title' className={classes.title}>
-              Result
-            </Typography>
-            <IconButton aria-label='Share'>
-              <ShareIcon className={classes.shareIcon} />
-            </IconButton>
-          </div>
-        }
-      />
-      {props.children}
-    </Card>
-  )
-}
+export const ResultCardHeader = ({ classes, children }) => (
+  <Card className={classes.card}>
+    <CardHeader
+      className={classes.header}
+      title={
+        <div className={classes.titleWrapper}>
+          <Typography type='title' className={classes.title}>
+            Result
+          </Typography>
+          <IconButton aria-label='Share'>
+            <ShareIcon className={classes.shareIcon} />
+          </IconButton>
+        </div>
+      }
+    />
+    {children}
+  </Card>
+)
