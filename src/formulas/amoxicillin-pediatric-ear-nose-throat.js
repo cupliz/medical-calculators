2bbafb62c5cb7cf02fe1fd0a37bea5b6f16ad339
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-19 20:04:42 +1000

    use default content

--- src/formulas/amoxicillin-pediatric-ear-nose-throat.js
+++ src/formulas/amoxicillin-pediatric-ear-nose-throat.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import Card, { CardContent, CardHeader } from 'material-ui/Card'
+import IconButton from 'material-ui/IconButton'
+import ShareIcon from 'material-ui-icons/Share'
+import Typography from 'material-ui/Typography'
 
 const formula = (weight, dosage) => {
   // Dose = Weight * Dosage
   return weight * dosage
 }
 
-const Formula = props => <div>inside Formula</div>
-
+const Formula = props => {
+  const classes = props
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        className={classes.header}
+        title={
+          <div className={classes.titleWrapper}>
+            <Typography type='title' className={classes.title}>
+              Result
+            </Typography>
+            <IconButton aria-label='Share'>
+              <ShareIcon className={classes.shareIcon} />
+            </IconButton>
+          </div>
+        }
+      />
+      <CardContent className={classes.content}>
+        <Typography type='caption' className={classes.contentText}>
+          Points
+        </Typography>
+        <Typography type='title' className={classes.contentText}>
+          123
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
 export default Formula
