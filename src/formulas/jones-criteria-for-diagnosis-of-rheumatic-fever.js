b5fb5188cf879ad22a5450b25c39fd465c6ecd7c
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-23 15:01:13 +1000

    extract few vars

--- src/formulas/jones-criteria-for-diagnosis-of-rheumatic-fever.js
+++ src/formulas/jones-criteria-for-diagnosis-of-rheumatic-fever.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { CardContent } from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import { ResultCardHeader } from '../components/Calculator/body/calculate/results/ResultCardHeader'
+import MenuItem from 'material-ui/Menu/MenuItem'
+import TextField from 'material-ui/TextField'
+import Button from 'material-ui/Button'
+import AddIcon from 'material-ui-icons/Add'
+import RemoveIcon from 'material-ui-icons/Remove'
 
-const Formula = props => (
-  <div>
-    Inside Formula Comp
-  </div>
-)
+class FormulaComponent extends Component {
+  state = {}
 
-export default Formula
+  render () {
+    const { classes, data } = this.props
+    const { questions } = data
+
+    // extract needed field vars
+    let requiredCriteria = null
+    let majorCriteria = null
+    let minorCriteria = null
+
+    return <p>Inside formula</p>
+  }
+}
+export default FormulaComponent
