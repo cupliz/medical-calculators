7282c739bdff8ffede4963728cacbc8c9a740c49
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-26 15:59:02 +1000

    extract/add things

--- src/components/Decimal/Decimal.js
+++ src/components/Decimal/Decimal.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import Button from 'material-ui/Button'
+import AddIcon from 'material-ui-icons/Add'
+import RemoveIcon from 'material-ui-icons/Remove'
+import Typography from 'material-ui/Typography'
 
-const Decimal = props => (
-  <div>
-    Inside Decimal comp
+const Decimal = ({ classes, decimal, onDecimalChange }) => (
+  <div className={classes.decimalPrecisionWrapper}>
+    <Typography type='title' className={classes.decimalPrecision}>
+      Decimal Precision
+    </Typography>
+    <div className={classes.decimalButtonsWrapper}>
+      <Button
+        fab
+        color='primary'
+        aria-label='add'
+        className={classes.decimalButton}
+        onClick={() => onDecimalChange('-')}
+      >
+        <RemoveIcon />
+      </Button>
+      <Typography type='title' className={classes.decimalPoint}>
+        {decimal}
+      </Typography>
+      <Button
+        fab
+        color='primary'
+        aria-label='add'
+        className={classes.decimalButton}
+        onClick={() => onDecimalChange('+')}
+      >
+        <AddIcon />
+      </Button>
+    </div>
   </div>
 )
 
