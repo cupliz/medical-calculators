f389147a280b45868d38fc3ee2e95dff63acc267
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-26 15:26:58 +1000

    use showPoints

--- src/components/Calculator/body/calculate/results/ResultCardPoints.js
+++ src/components/Calculator/body/calculate/results/ResultCardPoints.js
@@ -4,18 +4,29 @@ import Typography from 'material-ui/Typography'
 import { ResultCardHeader } from './ResultCardHeader'
 
 const ResultCardPoints = ({classes, points, results, showPointsInResults}) => {
+  const renderPoints = () => {
+    if (showPointsInResults === false) {
+      return null
+    } else {
+      return (
+        <div>
+          <Typography type='caption' className={classes.contentText}>
+            Points
+          </Typography>
+          <Typography type='title' className={classes.contentText}>
+            {points}
+          </Typography>
+        </div>
+      )
+    }
+  }
   if (points === 0 && !results) {
     return null
   } else {
     return (
       <ResultCardHeader classes={classes}>
         <CardContent className={classes.content}>
-          {showPointsInResults && <Typography type='caption' className={classes.contentText}>
-            Points
-          </Typography> }
-          {showPointsInResults && <Typography type='title' className={classes.contentText}>
-            {points}
-          </Typography> }
+          {renderPoints()}
           {results &&
             results.map(item => (
               <Typography
