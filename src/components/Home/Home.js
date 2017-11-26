eb29c8e950bba1d61a1cc087313040e0aeead30e
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-26 16:31:47 +1000

    add Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
+  renderList = list => {
+    return list.map(listItem => (
+      <li key={listItem.id}>
+        <Link to={`/${listItem.id}`}>{listItem.title}</Link>
+      </li>
+    ))
+  }
+
   render () {
-    return (
-      <div>
-        Inside Home
-      </div>
-    )
+    return <ul>{this.renderList(this.props.data)}</ul>
   }
 }
 
