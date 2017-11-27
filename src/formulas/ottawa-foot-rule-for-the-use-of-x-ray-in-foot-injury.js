386b32587b8deb61cf4286cb1448c589e85b9e00
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-27 15:07:23 +1000

    add ottawa foot

---
+++ src/formulas/ottawa-foot-rule-for-the-use-of-x-ray-in-foot-injury.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import { CardContent } from 'material-ui/Card'
+import Typography from 'material-ui/Typography'
+import { ResultCardHeader } from '../components/Calculator/body/calculate/results/ResultCardHeader'
+
+const getCalculateGroupPoints = calculate => {
+  return calculate
+    .map(item => parseFloat(item.points))
+    .reduce((previousValue, currentValue) => previousValue + currentValue)
+}
+
+class FormulaComponent extends Component {
+  state = {}
+
+  handleFormulaCalc = (majorCriteria, minorCriteria, classes) => {
+    // Diagnostic : 1 Major Criteria and 1 Minor Criteria
+    //
+    // Diagnostic : 0 Major Criteria and 3 Minor Criteria
+    if (
+      (majorCriteria >= 1 && minorCriteria >= 1) ||
+      (majorCriteria >= 0 && minorCriteria >= 3)
+    ) {
+      return (
+        <Typography type='title' className={classes.contentText}>
+          Diagnostic with:
+        </Typography>
+      )
+    } else {
+      return (
+        <Typography type='title' className={classes.contentText}>
+          Insufficient with:
+        </Typography>
+      )
+    }
+  }
+
+  render () {
+    const { classes, data } = this.props
+    const { questions } = data
+
+    // extract needed field vars
+    let majorCriteria = 0
+    let minorCriteria = 0
+
+    questions.map((question, index) => {
+      const { calculate } = question
+      if (calculate) {
+        if (index === 0) {
+          majorCriteria = getCalculateGroupPoints(calculate)
+        }
+        if (index === 1) {
+          minorCriteria = getCalculateGroupPoints(calculate)
+        }
+      }
+      return calculate
+    })
+
+    if (majorCriteria || minorCriteria) {
+      return (
+        <ResultCardHeader classes={classes}>
+          <CardContent className={classes.content}>
+            {this.handleFormulaCalc(majorCriteria, minorCriteria, classes)}
+            <div className={classes.resultWrapper}>
+              <Typography type='title' className={classes.resultTextNoBold}>
+                {majorCriteria} Major Criteria
+              </Typography>
+            </div>
+            <div className={classes.resultWrapper}>
+              <Typography type='title' className={classes.resultTextNoBold}>
+                {minorCriteria} Minor Criteria
+              </Typography>
+            </div>
+          </CardContent>
+        </ResultCardHeader>
+      )
+    } else {
+      return null
+    }
+  }
+}
+
+export default FormulaComponent
