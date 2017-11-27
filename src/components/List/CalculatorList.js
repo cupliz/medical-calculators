521a27312777035eba29ff22fd9585b2fd830ad8
Govard Barkhatov <gbarkhatov@gmail.com>
2017-11-27 15:31:14 +1000

    add render List

--- src/components/List/CalculatorList.js
+++ src/components/List/CalculatorList.js
@@ -11,6 +11,14 @@ const styles = theme => ({
   }
 })
 
+const renderList = list => {
+  return list.map(listItem => (
+    <li key={listItem.id}>
+      <Link to={`/${listItem.id}`}>{listItem.title}</Link>
+    </li>
+  ))
+}
+
 const CalculatorList = (props) => {
   const { classes } = props
   return (
